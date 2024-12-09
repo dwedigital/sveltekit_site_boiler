687d9b6084b71b559c10d5e6fbca01f82794c27e
Dave Edwards <dave@dwedigital.com>
2024-12-09 19:13:42 +0000

    changed

--- src/routes/about/+page.server.js
+++ src/routes/about/+page.server.js
@@ -1,4 +1,4 @@
-import { env } from "$env/dynamic/private";
+import { env } from "$env/static/private";
 import { Resend } from "resend";
 
 export const actions = {
@@ -9,11 +9,11 @@ export const actions = {
   },
 };
 
-const sendEmail = async (data, platform) => {
+const sendEmail = async (data) => {
   // get the name from the form data
   const name = data.get("name");
   const msg = data.get("message");
-  const resend = new Resend(platform.env.RESEND_API);
+  const resend = new Resend(env.RESEND_API);
   const { resp, error } = await resend.emails.send({
     to: "dave@dwedigital.com",
     from: "dave@email.dwedigital.com",
